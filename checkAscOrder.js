2e9e13f18b75b4ad62f10f8036c2ee229bac1d84
Alex <eieghtbitdolphin@gmail.com>
2018-09-12 07:20:20 -1000

    This is pretty Good

---
+++ checkAscOrder.js
@@ -0,0 +1,8 @@
+var arrNum = [10,23,50,5,24,94,35,65,45,89,76];
+var arrStr = ["Little_Boy","Super","Turn","Moon","oof"];
+
+var checkAscOrder = function(arr) {
+    return arr.sort();
+}
+
+console.log(checkAscOrder(arrStr));
